9489f57abec8276002f756d4f5c5728b32584ef0
Parvez-pabel <pabelalomparvez2233@gmail.com>
2025-07-11 17:18:38 +0600

    rebuild

--- client/src/store/ProductStore.js
+++ client/src/store/ProductStore.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 import { create } from "zustand";
+import axiosInstance from "../api/axiosInstance";
 
 const ProductStore = create((set) => ({
   BrandList: null,
   BrandListRequest: async () => {
-    let res = await axios.get(`api/ProductBrandList`);
+    let res = await axiosInstance.get(`/ProductBrandList`);
     if (res.data["status"] === "success") {
       set({ BrandList: res.data["data"] });
     }
   },
   CategoryList: null,
   CategoryListRequest: async () => {
-    let res = await axios.get(`/api/ProductCategoryList`);
+    let res = await axiosInstance.get(`/ProductCategoryList`);
     if (res.data["status"] === "success") {
       set({ CategoryList: res.data["data"] });
     }
   },
   SliderList: null,
   SliderListRequest: async () => {
-    let res = await axios.get(`/api/ProductSliderList`);
+    let res = await axiosInstance.get(`/ProductSliderList`);
     if (res.data["status"] === "success") {
       set({ SliderList: res.data["data"] });
     }
@@ -26,7 +27,7 @@ const ProductStore = create((set) => ({
   ProductListByRemark: null,
   ProductListByRemarkRequest: async (remark) => {
     set({ ProductListByRemark: null });
-    let res = await axios.get(`/api/ProductListByRemark/${remark}`);
+    let res = await axiosInstance.get(`/ProductListByRemark/${remark}`);
     if (res.data["status"] === "success") {
       set({ ProductListByRemark: res.data["data"] });
     }
@@ -34,28 +35,30 @@ const ProductStore = create((set) => ({
   ProductList: null,
   ProductListByBrandRequest: async (BrandID) => {
     set({ ProductList: null });
-    let res = await axios.get(`/api/ProductListByBrand/${BrandID}`);
+    let res = await axiosInstance.get(`/ProductListByBrand/${BrandID}`);
     if (res.data["status"] === "success") {
       set({ ProductList: res.data["data"] });
     }
   },
   ProductListByCategoryRequest: async (CategoryID) => {
     set({ ProductList: null });
-    let res = await axios.get(`/api/ProductListByCategory/${CategoryID}`);
+    let res = await axiosInstance.get(`/ProductListByCategory/${CategoryID}`);
     if (res.data["status"] === "success") {
       set({ ProductList: res.data["data"] });
     }
   },
   ProductListByKeyWordRequest: async (Keyword) => {
     set({ ProductList: null });
-    let res = await axios.get(`/api/ProductListByKeyword/${Keyword}`);
+    let res = await axiosInstance.get(`/ProductListByKeyword/${Keyword}`);
     if (res.data["status"] === "success") {
       set({ ProductList: res.data["data"] });
     }
   },
   ProductListBySimilarRequest: async (CategoryID) => {
     set({ ProductList: null });
-    let res = await axios.get(`/api/ProductListBySimilar/${CategoryID}`);
+    let res = await aaxiosInstancexios.get(
+      `/ProductListBySimilar/${CategoryID}`
+    );
     if (res.data["status"] === "success") {
       set({ ProductList: res.data["data"] });
     }
@@ -67,7 +70,7 @@ const ProductStore = create((set) => ({
   },
   ProductListByFilterRequest: async (postBody) => {
     set({ ProductList: null });
-    let res = await axios.post(`/api/ProductListByFilter`, postBody);
+    let res = await axiosInstance.post(`/ProductListByFilter`, postBody);
     if (res.data["status"] === "success") {
       set({ ProductList: res.data["data"] });
     }
@@ -75,7 +78,7 @@ const ProductStore = create((set) => ({
   ProductDetails: null,
   ProductDetailsRequest: async (ProductID) => {
     set({ ProductDetails: null });
-    let res = await axios.get(`/api/ProductDetails/${ProductID}`);
+    let res = await axiosInstance.get(`/ProductDetails/${ProductID}`);
     if (res.data["status"] === "success") {
       set({ ProductDetails: res.data["data"] });
     }
@@ -83,7 +86,7 @@ const ProductStore = create((set) => ({
   ProductReview: null,
   ProductReviewRequest: async (ProductID) => {
     set({ ProductReview: null });
-    let res = await axios.get(`/api/ProductReviewList/${ProductID}`);
+    let res = await axiosInstance.get(`/ProductReviewList/${ProductID}`);
     if (res.data["status"] === "success") {
       set({ ProductReview: res.data["data"] });
     }
